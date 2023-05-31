68d0a723d4ff116d9b8bb854b461c2cdbd15805a
Aries <hkn406532@163.com>
2023-05-31 15:31:29 +0800

    纯净版

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const electron_1 = require("electron");
-const mainWindow_1 = require("./mainWindow");
-electron_1.app.on('ready', () => {
-    process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'; // 关闭web安全警告
-    (0, mainWindow_1.create)();
-});
